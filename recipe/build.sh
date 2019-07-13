28a40aa0498c22aa5a16cff360529e1ca47e0d5b
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-07-12 22:32:37 -0600

    sysroot

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [[ $(uname) == Darwin ]]; then
+  export CFLAGS="${CFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
+  export CXXFLAGS="${CFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
+fi
+
 $CXX $CPPFLAGS $CXXFLAGS $LIBS $LDFLAGS testme.cpp -o testme
 mkdir -p $PREFIX/bin
 cp testme $PREFIX/bin/testme
