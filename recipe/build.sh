614370e68e795333b74fc43fe7cff1b6e30230f3
Giovanni Bussi <giovanni.bussi@gmail.com>
2020-01-24 00:35:19 +0100

    Revert "test without SDKROOT"
    This reverts commit 79fbdce0946d4f804780251a690d1e66baebcf20.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,9 @@
 set -e
 set -x
 
-#if [[ $(uname) == Darwin ]]; then
-  #export SDKROOT="${CONDA_BUILD_SYSROOT}"
-#fi
+if [[ $(uname) == Darwin ]]; then
+  export SDKROOT="${CONDA_BUILD_SYSROOT}"
+fi
 
 $CXX $CPPFLAGS $CXXFLAGS $LIBS $LDFLAGS testme.cpp -o testme
 mkdir -p $PREFIX/bin
